c9040b0b6967d42c112eedc7a036270cf64ed519
Your Name <you@example.com>
2024-09-15 19:27:30 +0100

    you

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 """distributes an archive to your web servers"""
-from fabric.api import env, put, sudo
+from fabric.api import env, put, run
 import os
+
+env.hosts = ['54.175.134.91', '100.25.104.180']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """Do_deploy"""
 
     if not os.path.exists(archive_path):
+        print('I exit here')
         return False
     try:
+        print('I get in here')
         arch_name = archive_path.split('/')[-1]
         arch_wext = arch_name.split('.')[0]
 
         put(archive_path, f"/tmp/{arch_name}")
-        sudo(f"mkdir -p /data/web_static/releases/{arch_wext}/")
-        sudo(f"tar -xzf /tmp/{arch_name} \
+        run(f"mkdir -p /data/web_static/releases/{arch_wext}/")
+        run(f"tar -xzf /tmp/{arch_name} \
                 -C /data/web_static/releases/{arch_wext}/")
-        sudo(f"rm /tmp/{arch_name}")
-        sudo(f"mv /data/web_static/releases/{arch_wext}/web_static/* \
+        run(f"rm /tmp/{arch_name}")
+        run(f"mv /data/web_static/releases/{arch_wext}/web_static/* \
                 /data/web_static/releases/{arch_wext}/")
-        sudo(f"rm -rf /data/web_static/releases/{arch_wext}/web_static")
-        sudo(f"rm -rf /data/web_static/current")
-        sudo(f"ln -s /data/web_static/releases/{arch_wext} \
+        run(f"rm -rf /data/web_static/releases/{arch_wext}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s /data/web_static/releases/{arch_wext} \
                 /data/web_static/current")
         return True
     except Exception as e:

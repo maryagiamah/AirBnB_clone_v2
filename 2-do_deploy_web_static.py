6163572c576a9f442d3222083f145074e6eb8ee3
Your Name <you@example.com>
2024-09-15 19:53:41 +0100

    you

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,17 +3,16 @@
 from fabric.api import env, put, run
 import os
 
-env.hosts = ['xx-web-01', 'xx-web-02']
+env.hosts = ['54.175.134.91', '100.25.104.180']
+env.user = ['ubuntu']
 
 
 def do_deploy(archive_path):
     """Do_deploy"""
 
     if not os.path.exists(archive_path):
-        print('I exit here')
         return False
     try:
-        print('I get in here')
         arch_name = archive_path.split('/')[-1]
         arch_wext = arch_name.split('.')[0]
 
@@ -26,7 +25,7 @@ def do_deploy(archive_path):
                 /data/web_static/releases/{arch_wext}/")
         run(f"rm -rf /data/web_static/releases/{arch_wext}/web_static")
         run(f"rm -rf /data/web_static/current")
-        run(f"ln -s /data/web_static/releases/{arch_wext} \
+        run(f"ln -s /data/web_static/releases/{arch_wext}/ \
                 /data/web_static/current")
         return True
     except Exception as e:

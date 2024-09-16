1382b190b6f46c14db5baec940ea5edb6317fc56
Your Name <you@example.com>
2024-09-16 08:31:10 +0100

    you

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,25 +13,19 @@ def do_deploy(archive_path):
 
     if not os.path.exists(archive_path):
         return False
-
     try:
         arch_name = archive_path.split('/')[-1]
         arch_wext = arch_name.split('.')[0]
+        path = '/data/web_static/releases/'
 
-        put(archive_path, f"/tmp/")
-        sudo(f"mkdir -p /data/web_static/releases/{arch_wext}/")
-        sudo(f"tar -xzf /tmp/{arch_name} -C \
-                /data/web_static/releases/{arch_wext}/")
+        put(archive_path, "/tmp/")
+        sudo(f"mkdir -p {path}{arch_wext}")
+        sudo(f"tar -xzf /tmp/{arch_name} -C {path}{arch_wext}")
         sudo(f"rm /tmp/{arch_name}")
-        sudo(f"mv /data/web_static/releases/{arch_wext}/web_static\
-                /* /data/web_static/releases/{arch_wext}/")
-        sudo(f"rm -rf /data/web_static/releases\
-                /{arch_wext}/web_static")
-        sudo(f"rm -rf /data/web_static/current")
-        sudo(f"ln -s /data/web_static/releases/{arch_wext}/ \
-                /data/web_static/current")
-
+        sudo(f"mv {path}{arch_wext}/web_static/* {path}{arch_wext}")
+        sudo(f"rm -rf {path}{arch_wext}/web_static")
+        sudo("rm -rf /data/web_static/current")
+        sudo(f"ln -s {path}{arch_wext} /data/web_static/current")
         return True
-
     except Exception as e:
         return False

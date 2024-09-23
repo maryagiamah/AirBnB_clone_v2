9e8d05a3edf26451dadcd9b85140e8c9e6e4ddcc
Your Name <you@example.com>
2024-09-23 18:53:54 +0100

    you

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Flask web application"""
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """Home"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)

a4af8fd4398faf818d3d27ba670db86fb9bc2be6
Your Name <you@example.com>
2024-09-23 21:24:37 +0100

    you

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,7 +30,7 @@ def py_text(text="is cool"):
     return f"Python {text.replace('_', ' ')}"
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def n_int(n):
     """Integer"""
     if type(n) is int:

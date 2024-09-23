b84bcc23525f89670b05aef05c3ad031942a35cb
Your Name <you@example.com>
2024-09-23 20:48:39 +0100

    you

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,5 +17,11 @@ def home_hbnb():
     return "HBNB"
 
 
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """C text"""
+    return f"C {text.replace('_', ' ')}"
+
+
 if __name__ == '__main__':
     app.run(debug=True)

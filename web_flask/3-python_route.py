f591d291370d062af273dee8b4bd37bc6a77154c
Your Name <you@example.com>
2024-09-23 21:00:21 +0100

    you

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Home"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def home_hbnb():
+    """Home"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """C text"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def py_text(text="is cool"):
+    """Python text"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

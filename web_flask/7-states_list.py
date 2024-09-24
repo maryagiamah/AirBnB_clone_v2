a0c275939fdced0883a9b7b781de20fd03941c18
Your Name <you@example.com>
2024-09-24 17:09:21 +0100

    you

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """States List"""
-    return render_template('7-states_list.html', states=storage.all())
+    return render_template('7-states_list.html', states=storage.all(State))
 
 
 @app.teardown_appcontext

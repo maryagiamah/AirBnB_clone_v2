f3e2d3af813e3d0ddfe7af6198f24c214ccd6a07
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-11 23:51:52 +0100

    you

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,15 +11,15 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """States List"""
-
-    return render_template('7-states_list.html', states=storage.all('State'))
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tearDown(exep):
+def tearDown(self):
     """Close"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
